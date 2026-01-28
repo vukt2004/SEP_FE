e6e65e3177a4ff5ad70be22c4b7fb78c70882c4f
Nguyen Duc Hoang Vu <tatcatronghuvo@gmail.com>
2026-01-28 16:52:10 +0700

    Add Tailwind set up

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { fileURLToPath, URL } from "node:url";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
