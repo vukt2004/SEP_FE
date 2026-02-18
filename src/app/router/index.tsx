cc19d5ea2feab91fb4d9b716d0112cca34dcf1b3
Nguyen Duc Hoang Vu <tatcatronghuvo@gmail.com>
2026-02-18 22:57:46 +0700

    Add more file

--- src/app/router/index.tsx
+++ src/app/router/index.tsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import HomePage from "@/shared/pages/Home";
-import GameView from "../shared/features/game-view/GameView";
-import PlatformGameView from "../shared/features/game-view/PlatformGameView";
+import GameView from "../../shared/features/game-view/GameView";
+import PlatformGameView from "../../shared/features/game-view/PlatformGameView";
 
 export const router = createBrowserRouter([
   { path: "/", element: <HomePage /> },
