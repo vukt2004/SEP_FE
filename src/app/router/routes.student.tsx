99f8c2faaba4c301cba28c02c73b7444eebc1833
NguyenTien142 <tiennnse181773@fpt.edu.vn>
2026-03-03 22:13:39 +0700

    add learner challenges menu page

--- src/app/router/routes.student.tsx
+++ src/app/router/routes.student.tsx
@@ -10,6 +10,7 @@ const StudentLayout = React.lazy(() => import("@/portals/student/layout/StudentL
 
 // Pages (student authenticated)
 const StudentHomePage = React.lazy(() => import("@/portals/student/pages/HomePage"));
+const StudentChallengesPage = React.lazy(() => import("@/portals/student/pages/ChallengesPage"));
 
 // Pages (student public)
 const StudentLoginPage = React.lazy(() => import("@/portals/student/pages/LoginPage"));
@@ -48,5 +49,6 @@ export const studentRoutes: RouteObject = {
   children: [
     { index: true, element: <StudentHomePage /> },
     { path: "home", element: <StudentHomePage /> },
+    { path: "challenges", element: <StudentChallengesPage /> },
   ],
 };
